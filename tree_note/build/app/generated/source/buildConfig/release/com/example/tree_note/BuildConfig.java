85292691fcf06e91c98839faca8183d675fd0000
Jade <jademmb@gmail.com>
2020-09-20 16:37:16 +1200

    1.0

--- tree_note/build/app/generated/source/buildConfig/release/com/example/tree_note/BuildConfig.java
+++ tree_note/build/app/generated/source/buildConfig/release/com/example/tree_note/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.tree_note;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.example.Treenotes";
+  public static final String APPLICATION_ID = "jade.martin.Treenotes";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
